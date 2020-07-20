751ed5db45eadd09da0df9622fd1f570da4a069b
nitat-n <nitat.n@rbru.ac.th>
2020-07-20 10:18:47 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace oop_id_w02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room r433 = new Room();
+            Room r434 = new Room("434","434",4,4,"Computer");
+            Console.WriteLine(r433);
+            Console.WriteLine(r434);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
